5db423b4e85c64d22546a08e5ba40273bd535e9a
Connor Ferguson <cpfergus1@gmail.com>
2020-09-17 12:11:40 -0600

    Added test for assigning key and date if input is blank

--- test/enigma_test.rb
+++ test/enigma_test.rb
@@ -5,13 +5,25 @@ require './lib/cipher.rb'
 class EnigmaTest <Minitest::Test
 
   def setup
-    @enigma = Enigma.new("hello world", "02715", "040895")
+    @enigma = Enigma.new('hello world', '02715', '040895')
   end
 
   def test_enigma_holds_values
-    assert_equal "hello world", @enigma.message
-    assert_equal "02715", @enigma.cipherkey
+    assert_equal 'hello world', @enigma.message
+    assert_equal '02715', @enigma.cipherkey
     assert_equal '040895', @enigma.cipherdate
+  end
+
+  def test_enigma_can_inject_key_and_date_if_blank
+
+    @enigma.fill_in_missing
+    assert_equal '02715', @enigma.cipherkey
+    assert_equal '040895', @enigma.cipherdate
+
+    enigma = Enigma.new('hello world')
+    enigma.fill_in_missing
+    assert_equal Time.now.strftime("%d%m%y"), enigma.cipherdate
+    assert_equal 5, enigma.cipherkey.length
   end
 
 end
