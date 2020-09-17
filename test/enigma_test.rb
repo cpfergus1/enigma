f6305be8313bafbd4e51f69576bca670270b5548
Connor Ferguson <cpfergus1@gmail.com>
2020-09-17 11:08:26 -0600

    Added test for enigma initialize

--- test/enigma_test.rb
+++ test/enigma_test.rb
@@ -1,5 +1,17 @@
 require 'test_helper'
 require './lib/enigma.rb'
+require './lib/cypher.rb'
 
 class EnigmaTest <Minitest::Test
+
+  def setup
+    @enigma = Enigma.new("hello world", "02715", "040895")
+  end
+
+  def test_enigma_holds_values
+    assert_equal "hello world", @enigma.message
+    assert_equal "02715", @enigma.cipherkey
+    assert_equal '040895', @enigma.cipherdate
+  end
+
 end
