eea15ac731370556ad4cd6ae547e063b1a196413
Connor Ferguson <cpfergus1@gmail.com>
2020-09-17 12:31:35 -0600

    Updated code to match interation pattern

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -2,15 +2,16 @@ class Enigma
 
   attr_reader :message, :cipherkey, :cipherdate
 
-  def initialize(message, cipherkey = '', cipherdate = '')
-    @message = message
-    @cipherkey = cipherkey
-    @cipherdate = cipherdate
+  def initialize
+
   end
 
-  def fill_in_blank
-    @cipherkey = (rand(10 ** 5)).to_s if cipherkey == ''
-    @cipherdate = Time.now.strftime("%d%m%y") if cipherdate == ''
+  def fill_in_cipherkey
+    cipherkey = (rand(10 ** 5)).to_s if cipherkey == nil
+  end
+
+  def fill_in_cipherdate
+    cipherdate = Time.now.strftime("%d%m%y") if cipherdate == ''
   end
 
 end
--- test/enigma_test.rb
+++ test/enigma_test.rb
@@ -5,25 +5,20 @@ require './lib/cipher.rb'
 class EnigmaTest <Minitest::Test
 
   def setup
-    @enigma = Enigma.new('hello world', '02715', '040895')
+    @enigma = Enigma.new
   end
 
-  def test_enigma_holds_values
-    assert_equal 'hello world', @enigma.message
-    assert_equal '02715', @enigma.cipherkey
-    assert_equal '040895', @enigma.cipherdate
+  def test_enigma_exists
+    assert Enigma, @enigma
   end
 
-  def test_enigma_can_inject_key_and_date_if_blank
-
-    @enigma.fill_in_blank
-    assert_equal '02715', @enigma.cipherkey
-    assert_equal '040895', @enigma.cipherdate
-
-    enigma = Enigma.new('hello world')
-    enigma.fill_in_blank
-    assert_equal Time.now.strftime("%d%m%y"), enigma.cipherdate
-    assert_equal 5, enigma.cipherkey.length
+  def test_enigma_can_create_cipherkey
+    assert_equal 5, enigma.fill_in_cipherkey.length
   end
+
+  def test_enigma_can_create_cipherdate
+    assert_equal Time.now.strftime("%d%m%y"), enigma.fill_in_cipherdate
+  end
+
 
 end
