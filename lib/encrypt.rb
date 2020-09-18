ecd7648388bb8ae7a28ac71f1f7007bf414fb54b
Connor Ferguson <cpfergus1@gmail.com>
2020-09-18 13:35:57 -0600

    Implemented encrypt runner file to generate encrypted text file

--- lib/encrypt.rb
+++ lib/encrypt.rb
@@ -1,22 +1,14 @@
-require_relative 'enigma'
+require './lib/enigma'
+require 'json'
 
-class Encrypt < Enigma
-  attr_reader :key_a, :alphabet
+message_to_encrypt = File.open(ARGV[0],'r')
+message = (message_to_encrypt.read).chomp
+message_to_encrypt.close
 
-  def initialize
-    @key_a = Array(:A..:D)
-    @alphabet = Array('a'..'z') + [' ']
-  end
+enigma = Enigma.new
+encryption = enigma.encrypt(message)
 
-  def encryption(message, cipherkey, cipherdate)
-    keys = generate_keys(total_shift(cipherkey, cipherdate))
-    { encryption: cipher(message.downcase, keys), key: cipherkey, date: cipherdate }
-  end
-
-  def total_shift(cipherkey, cipherdate)
-    generate_shift(cipherkey).merge(offsets(cipherdate)) do |_key, shift, offset|
-      shift + offset
-    end
-  end
-
-end
+encrypted = File.open(ARGV[1],'w')
+encrypted.write JSON.dump(encryption)
+encrypted.close
+puts "Created 'encrypted.txt with the key #{enigma.cipherkey} and date #{enigma.cipherdate}"
